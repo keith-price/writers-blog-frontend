465e4e3e74d1f9b91e5984cde7bef78cca57c1e6
Keith Price <keithprice.dev@gmail.com>
2023-04-11 21:55:21 +0700

    managed to useSanityQuery to get fetch all the post data

--- pages/index.vue
+++ pages/index.vue
@@ -11,6 +11,10 @@ const { data } = useSanityQuery(query);
       <ul v-if="data?.length">
         <li v-for="post in data" v-bind:key="data._id">
           <h3>{{ post.title }}</h3>
+          <p>{{ post.excerpt }}</p>
+          <p>{{ post.author }}</p>
+          <p>{{ post.category }}</p>
+          <SanityContent :blocks="post.content" />
           <img
             :src="$urlFor(post.coverImage).size(426).url()"
             :alt="post.title"
