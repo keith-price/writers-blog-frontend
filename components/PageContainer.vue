a1e2442e2caf389e0ce52130fe2e52c7cba124d9
Keith Price <keithprice.dev@gmail.com>
2023-04-17 10:35:03 +0700

    testing layout for homepage

--- components/PageContainer.vue
+++ components/PageContainer.vue
@@ -1,7 +1,5 @@
 <template>
-  <div
-    class="flex container justify-center align-middle max-w-7xl mx-auto px-6 lg:px-8 min-h-screen"
-  >
+  <div class="container max-w-7xl mx-auto px-6 lg:px-8 min-h-screen">
     <slot />
   </div>
 </template>
--- pages/index.vue
+++ pages/index.vue
@@ -11,6 +11,7 @@ const featureData = fetchFeaturedPosts();
         <li v-bind:key="latestData._id">
           <h2 class="text-3xl font-bold">Latest Post</h2>
           <h3>{{ latestData.title }}</h3>
+          <!-- SanityContent brings in the content body -->
           <!-- <SanityContent :blocks="latestData.content" /> -->
           <img
             :src="$urlFor(latestData.coverImage).size(1280, 720).url()"
@@ -26,25 +27,30 @@ const featureData = fetchFeaturedPosts();
       </ul>
       <p v-else>No Latest posts to show</p>
 
-      <!-- <h2>Featured Posts</h2>
-      <ul v-if="featureData?.length">
-        <li v-for="post in featureData" v-bind:key="featureData._id">
-          <h3>{{ post.title }}</h3>
-          <p>{{ post.excerpt }}</p>
-          <p>{{ post.author }}</p>
-          <p>{{ post.category }}</p>
-          <SanityContent :blocks="post.content" />
-          <img
-            :src="$urlFor(post.coverImage).size(1280, 720).url()"
-            :alt="post.title"
-            height="720"
-            width="1280"
-            loading="lazy"
-          />
-        </li>
-      </ul>
+      <section>
+        <h2>Featured Posts</h2>
+        <div>
+          <ul
+            v-if="featureData?.length"
+            class="grid grid-cols-1 gap-4 lg:grid-cols-2 lg:gap-8"
+          >
+            <li v-for="post in featureData" v-bind:key="featureData._id">
+              <h3>{{ post.title }}</h3>
+              <img
+                :src="$urlFor(post.coverImage).size(1280, 720).url()"
+                :alt="post.title"
+                loading="lazy"
+              />
+              <p>{{ post.excerpt }}</p>
+              <p>{{ post.author }}</p>
+              <p>{{ post.category }}</p>
+              <!-- <SanityContent :blocks="post.content" /> -->
+            </li>
+          </ul>
 
-      <p v-else>No featured posts to show</p> -->
+          <!-- <p v-else>No featured posts to show</p> -->
+        </div>
+      </section>
     </main>
   </div>
 </template>
