9f6ff04151e3d80d1ceea7ce9dfe0d2dbab671a6
Keith Price <keithprice.dev@gmail.com>
2023-06-01 10:51:08 +0700

    added grid layout to categories pages

--- pages/[...slug]/index.vue
+++ pages/[...slug]/index.vue
@@ -9,38 +9,33 @@ const { data: singlePost } = await useSanityQuery(query);
 </script>
 
 <template>
-  <!-- button to navigate back to previous page -->
-  <div
-    class="title-back-container flex justify-between align-middle mt-5 mb-5 lg:mt-20"
-  >
-    <p class="text-2xl font-bold">{{ singlePost.title }}</p>
-    <button
-      class="px-4 h-8 rounded-md text-white bg-slate-100 hover:bg-slate-200 transition-all"
-      @click="$router.back()"
+  <main class="text-slate-900 mt-5 mb-20 lg:mt-20 lg:mb-20">
+    <!-- button to navigate back to previous page -->
+    <div
+      class="title-back-container flex justify-between align-middle mt-5 mb-5 lg:mt-20"
     >
-      <BackArrow class="text-2xl text-slate-900" />
-    </button>
-  </div>
-  <img
-    :src="$urlFor(singlePost.coverImage).size(1280, 720).url()"
-    :alt="singlePost.title"
-    height="720"
-    width="1280"
-    loading="lazy"
-    class="mb-2 rounded shadow-md"
-  />
-  <SanityContent :blocks="singlePost.content" />
-  <p class="text-sm font-semibold text-justify lg:text-base">
-    {{ singlePost.author }}
-  </p>
-  <p class="text-sm text-justify italic lg:text-base">
-    {{ `# ${singlePost.category}` }}
-  </p>
+      <p class="text-2xl font-bold">{{ singlePost.title }}</p>
+      <button
+        class="px-4 h-8 rounded-md text-white bg-slate-100 hover:bg-slate-200 transition-all"
+        @click="$router.back()"
+      >
+        <BackArrow class="text-2xl text-slate-900" />
+      </button>
+    </div>
+    <img
+      :src="$urlFor(singlePost.coverImage).size(1280, 720).url()"
+      :alt="singlePost.title"
+      height="720"
+      width="1280"
+      loading="lazy"
+      class="mb-2 rounded shadow-md"
+    />
+    <SanityContent :blocks="singlePost.content" />
+    <p class="text-sm font-semibold text-justify lg:text-base">
+      {{ singlePost.author }}
+    </p>
+    <p class="text-sm text-justify italic lg:text-base">
+      {{ `# ${singlePost.category}` }}
+    </p>
+  </main>
 </template>
-
-<style lang="css" scoped>
-p:empty {
-  display: block;
-  height: 1em;
-}
-</style>
--- pages/short-stories/index.vue
+++ pages/short-stories/index.vue
@@ -1,32 +1,49 @@
 <script setup>
+import BackArrow from "~/components/Icons/BackArrow.vue";
+
 const { fetchByCategory } = useUtils();
 const data = fetchByCategory("short story");
 </script>
 
 <template>
   <div>
-    <header>Writers' Group Short Stories</header>
-    <main>
-      <h2>Short Stories</h2>
-      <ul v-if="data?.length">
-        <li v-for="post in data" v-bind:key="data._id">
-          <h3>{{ post.title }}</h3>
-          <p>{{ post.excerpt }}</p>
-          <p>{{ post.author }}</p>
-          <p>{{ post.category }}</p>
-          <SanityContent :blocks="post.content" />
-          <img
-            :src="$urlFor(post.coverImage).size(426).url()"
-            :alt="post.title"
-            height="426"
-            width="426"
-            loading="lazy"
-            class="mb-2 rounded shadow-md"
-          />
+    <main class="text-slate-900 mt-5 mb-20 lg:mt-20 lg:mb-20">
+      <div class="title-back-container flex justify-between align-middle mb-8">
+        <h1 class="text-3xl font-bold">Short Stories</h1>
+        <button
+          class="px-4 align-middle rounded-md text-white bg-slate-100 hover:bg-slate-200 transition-all"
+          @click="$router.back()"
+        >
+          <BackArrow class="text-2xl text-slate-900" />
+        </button>
+      </div>
+      <ul
+        v-if="data?.length"
+        class="grid grid-cols-1 gap-y-8 gap-x-4 lg:grid-cols-2 lg:gap-8"
+      >
+        <li v-for="post in data" v-bind:key="post._id">
+          <NuxtLink :to="post.slug.current">
+            <p class="text-1xl font-bold mb-2">{{ post.title }}</p>
+            <img
+              :src="$urlFor(post.coverImage).size(1280, 720).url()"
+              :alt="post.title"
+              loading="lazy"
+              class="mb-2 rounded shadow-md"
+            />
+            <p class="text-sm text-justify mb-2 lg:text-base">
+              {{ post.excerpt }}
+            </p>
+          </NuxtLink>
+          <p class="text-sm font-semibold text-justify lg:text-base">
+            {{ post.author }}
+          </p>
+          <p class="text-sm text-justify italic lg:text-base">
+            {{ `# ${post.category}` }}
+          </p>
         </li>
       </ul>
+
+      <p v-else>No Short Stories to show</p>
     </main>
   </div>
 </template>
-
-<style lang="css" scoped></style>
